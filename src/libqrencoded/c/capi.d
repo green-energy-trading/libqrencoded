7c6c4771c6671cd4d07861bc3aac1733f5c7d354
Vincent Brendel <vincent.brendel@gmail.com>
2015-04-15 09:34:15 +1000

    attempt at qr function wrapper

--- src/libqrencoded/c/capi.d
+++ src/libqrencoded/c/capi.d
@@ -1,5 +1,12 @@
 module libqrencoded.c.capi;
 
+QRcode = {
+  int version;
+  int width;
+  unsigned char *data;
+}
+
 extern(C) {
 	char *QRcode_APIVersionString();
+  QRcode *QRcode_encodeString(const char *string, int version, QRecLevel level, QRencodeMode hint, int casesensitive);
 }
--- src/libqrencoded/encode.d
+++ src/libqrencoded/encode.d
@@ -2,9 +2,23 @@ module libqrencoded.encode;
 
 import libqrencoded.c.capi;
 
+importu std.array;
 import core.stdc.string;
 
 string libQREncodedVersion() {
-  auto res = QRcode_APIVersionString;
-  return cast(string)(cast(char[])res[0..strlen(res)]);
+	auto res = QRcode_APIVersionString;
+	return cast(string)(cast(char[])res[0..strlen(res)]);
+};
+
+bool[][] encodeQR(string s) {
+	bool[][] res;
+	auto qr = QRcode_encodeString(szSourceSring, 0, 3, 2, 1);
+	res.length = qr->width;
+	int i, j;
+	for (i = 0; i < qr->width; i++) {
+		res[i].length = qr->width;
+		for (j = qr->width - 1; j >= 0; j--) {
+			res[i][j] = qr->data[(j * qr->width) + i] & 0x1
+		}
+	}
 }
